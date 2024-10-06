aee667d4ce46ac18438f0d16b70823a82fa4b918
sam4b <sambarringer2004@protonmail.com>
2024-10-06 03:40:30 +0100

    add background and proper sphere intersection

--- Vec3.hs
+++ Vec3.hs
@@ -1,14 +1,14 @@
 module Vec3 where
 
-data Vec3 = Vec3 Float Float Float 
+data Vec3 = Vec3 Double Double Double 
 
 instance Show Vec3 where
     show (Vec3 x y z) = show x ++ ", " ++ show y ++ ", " ++ show z
 
-dotProduct :: Vec3 -> Vec3 -> Float
+dotProduct :: Vec3 -> Vec3 -> Double
 dotProduct (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = (x1 * x2) + (y1 * y2) + (z1 * z2)
 
-multiplyVector :: Vec3 -> Float -> Vec3
+multiplyVector :: Vec3 -> Double -> Vec3
 multiplyVector (Vec3 x y z) a = Vec3 (x * a) (y * a) (z * a)
 
 unitVector :: Vec3 -> Vec3
@@ -19,3 +19,7 @@ addVector (Vec3 a b c) (Vec3 d e f) = Vec3 (a + d) (b + e) (c + f)
 
 subtractVector :: Vec3 -> Vec3 -> Vec3
 subtractVector (Vec3 a b c) (Vec3 d e f) = Vec3 (a - d) (b - e) (c - f)
+
+vec3ToString :: Vec3 -> String
+vec3ToString (Vec3 x y z) = show (round (x * 255.999)) ++ " " ++ show (round (y*255.999)) ++ " " ++ show (round (z*255.999))
+        
--- main.hs
+++ main.hs
@@ -5,63 +5,89 @@ import Data.List
 import Data.Function
 
 import Vec3
+import Distribution.Simple.Test (test)
 
-data Sphere = Sphere Float Vec3
+data Sphere = Sphere Double Vec3
 data Ray = Ray Vec3 Vec3
 
 testIntersection :: Sphere -> Ray -> Maybe Float
 testIntersection (Sphere radius center) (Ray origin direction) 
-    | discriminant < 0 = Nothing
-    | otherwise = Just t
+    | discriminant < 0 = Nothing 
+    | otherwise = Just 5 --TBA
   where
     oc = subtractVector center origin
     a = dotProduct direction direction
-    b = -2.0 * dotProduct direction oc
-    c = dotProduct oc oc - radius * radius
+    b = -2.0 * (dotProduct direction oc)
+    c = (dotProduct oc oc) - (radius * radius)
     discriminant = b * b - 4 * a * c
-    t = (-b - sqrt discriminant) / (2 * a)
 
-rayAt :: Ray -> Float -> Vec3
+
+color :: Maybe Float -> Ray -> Vec3
+color (Just t) _ = Vec3 1 0 0
+color Nothing (Ray origin direction) = (multiplyVector (Vec3 1.0 1.0 1.0) (1.0-a)) `addVector` (multiplyVector (Vec3 0.5 0.7 1.0) a)
+    where
+        a = 0.5 * (y + 1)
+        unitDir = unitVector direction 
+        (Vec3 _ y _ ) = unitDir
+    
+rayAt :: Ray -> Double -> Vec3
 rayAt (Ray origin direction) t = addVector origin (multiplyVector direction t)
 
-color :: Ray -> Maybe Float -> Vec3
-color ray (Just t) = unitVector (rayAt ray t) `subtractVector` (Vec3 0 0 (-1))
-color ray Nothing = Vec3 0 0 0
+calculatePixelCenter :: Vec3 -> Vec3 -> Vec3 -> Double -> Double -> Vec3
+calculatePixelCenter pixelLoc pixeldu pixeldv x y = pixelLoc `addVector` (multiplyVector pixeldu x) `addVector` (multiplyVector pixeldv y)
 
+getRayDirection :: Vec3 -> Vec3 -> Vec3
+getRayDirection pixelCenter cameraCenter = pixelCenter `subtractVector` cameraCenter
 
-intersects :: Ray -> [Sphere] -> [Maybe Float]
-intersects ray [] = []
-intersects ray (sphere:spheres) = [testIntersection sphere ray] ++ intersects ray spheres
-
-runIntersectionTests :: Ray -> [Sphere] -> Maybe Float
-runIntersectionTests ray (sphere:spheres) 
-    | null intersections = Nothing
-    | otherwise = Just (head (sort intersections))
-    where 
-        intersections = [x | Just x <- intersects ray (sphere:spheres)]
-
-
-rayTrace :: [(Float, Float)] -> [Sphere] -> [Vec3]
-rayTrace [] _ = []
-rayTrace (coord:coords) spheres = [col] ++ rayTrace coords spheres
+rayTrace :: [(Double, Double)] -> Sphere -> (Double -> Double -> Vec3) -> Vec3 -> [Vec3] 
+rayTrace [] _ _ _ = []
+rayTrace (coord:coords) sphere pixelCenterFuncton cameraCenter = col : rayTrace coords sphere pixelCenterFuncton cameraCenter
     where
-        ray = Ray (Vec3 (fst coord) (snd coord) 0) (Vec3 0 0 (-1))
-        col = color ray (runIntersectionTests ray spheres)
+        ray = (Ray cameraCenter direction)
+        direction = getRayDirection pixelCenter cameraCenter
+        pixelCenter = pixelCenterFuncton (snd coord) (fst coord) 
+        col = color (testIntersection sphere ray) ray
 
-vec3ToString :: Vec3 -> String
-vec3ToString (Vec3 x y z) = show (round (x * 255.999)) ++ " " ++ show (round (y*255.999)) ++ " " ++ show (round (z*255.999))
-        
-writeListToFile :: FilePath -> [Vec3] -> IO ()
-writeListToFile filePath items = do
+writeListToFile :: FilePath -> [Vec3] -> Double -> Double -> IO ()
+writeListToFile filePath items width height = do
     handle <- openFile filePath WriteMode
-    mapM_ (hPutStrLn handle) ["P3\n256 256\n255"]
+    mapM_ (hPutStrLn handle) (["P3\n"] ++ [show width] ++ [" "] ++ [show height] ++ ["\n255"])
     mapM_ (hPutStrLn handle . vec3ToString) items
     hClose handle
 
+
+
 main :: IO ()
 main = do
-    let pixelCoords = [(x, y) | x <- [1..256], y <- [1..256]]
-    let spheres = [Sphere 5 (Vec3 0 0 (-1)) , Sphere 15 (Vec3 15 15 (-1)), Sphere 15 (Vec3 50 50 (-50))]
-    let colors = rayTrace pixelCoords spheres
-    writeListToFile "output.ppm" colors
+
+    let imageWidth :: Double = 400.0
+    let imageHeight :: Double = 225.0
+
+--(0,0), (0,1), ... (0, 225), (1, 0) , ... (1, 225)
+    let pixelCoords = [(y,x) | y <- [0..imageHeight-1], x <- [0..imageWidth-1]]
+    let sphere = Sphere 0.5 (Vec3 0 0 (-1))
+
+    let viewportHeight ::Double = 2.0
+    let viewportWidth = viewportHeight * (imageWidth / imageHeight)
+
+    let cameraCenter = (Vec3 0 0 0)
+
+    let viewportU = (Vec3 viewportWidth 0 0)
+    let viewportV = (Vec3 0 (-1 * viewportHeight) 0)
+
+    let pixeldu = multiplyVector viewportU (1.0 / imageWidth) 
+
+    let pixeldv = multiplyVector viewportV (1.0 / imageHeight) 
+
+    let focalLength = 1.0
+
+    let viewportUpperLeft = cameraCenter `subtractVector` (Vec3 0 0 focalLength) `subtractVector` (multiplyVector viewportU 0.5) `subtractVector` (multiplyVector viewportV 0.5)
+
+    let pixelLoc = addVector viewportUpperLeft (multiplyVector (pixeldu `addVector` pixeldv) 0.5)
+
+    let pixelF = calculatePixelCenter pixelLoc pixeldu pixeldv --Partially apply so I don't have to pass all that around.
+
+    let result = rayTrace pixelCoords sphere pixelF cameraCenter
+
+    writeListToFile "output.ppm" result imageWidth imageHeight
     
